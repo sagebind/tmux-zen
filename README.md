03a3db57f2e587e258c58f179bc81dfbae7aaec6
Stephen Coakley <me@stephencoakley.com>
2015-09-27 01:06:44 -0500

    Initial version ported from dotfiles

---
+++ README.md
@@ -0,0 +1,43 @@
+<div align="center">
+  <a href="http://github.com/oh-my-fish/oh-my-fish">
+  <img width=90px  src="https://cloud.githubusercontent.com/assets/8317250/8510172/f006f0a4-230f-11e5-98b6-5c2e3c87088f.png">
+  </a>
+</div>
+<br>
+
+# tmux zen
+Plugin for [Oh My Fish][omf-link] to tap into the zen power of [tmux](https://tmux.github.io).
+
+## Install
+You will first need to install tmux. The best way is usually with your operating system's package manager. Otherwise, you can download and compile tmux yourself:
+
+```fish
+$ wget https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz
+$ tar xcf tmux-2.0.tar.gz; and cd tmux-2.0
+$ ./configure; and make
+$ sudo make install
+```
+
+Now you can install tmux zen using [Oh My Fish][omf-link]:
+
+```fish
+$ omf install tmux-zen
+```
+
+That's it! Prepare yourself for an epic journey to ultimate terminal zen.
+
+## The path to zen
+It's a mystery...
+
+(I'll add a lengthy tutorial here soon.)
+
+# License
+[MIT][mit] © [coderstephen][author] et [al][contributors]
+
+
+[mit]:            http://opensource.org/licenses/MIT
+[author]:         http://github.com/coderstephen
+[contributors]:   https://github.com/coderstephen/pkg-tmux-zen/graphs/contributors
+[omf-link]:       https://www.github.com/oh-my-fish/oh-my-fish
+
+[license-badge]:  https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square
---
+++ tmux-zen.fish
@@ -0,0 +1,42 @@
+# Initialize the current fish session and connect to the tmux session.
+function init --on-event init_tmux-zen
+    if not set -q __tmux_session_name
+        set -U __tmux_session_name local
+    end
+
+    # Connect to the TMUX session if it exists, or create it if it doesn't.
+    if begin; [ "$TERM" = "screen" ]; not set -q TMUX; end
+        if tmux has-session -t $__tmux_session_name
+            exec tmux new-session -t $__tmux_session_name \; set destroy-unattached on \; new-window
+        else
+            exec tmux new-session -s $__tmux_session_name
+        end
+    else
+        # Set useful tmux variables.
+        set -g TMUX_WINDOW (tmux display-message -p '#I')
+
+        # Initialize the session if we didn't already.
+        if not set -q __tmux_is_initialized
+            __tmux_init
+        end
+    end
+end
+
+function __tmux_set -d "Sets a variable for all tmux windows"
+    if not set -q argv[2]
+        set argv[2] ""
+    end
+
+    # Set the variable for the current window as well as all future windows.
+    set -gx $argv[1] $argv[2]
+    tmux setenv -g -t $__tmux_session_name $argv[1] $argv[2]
+end
+
+function __tmux_init -d "Initializes the tmux session and applies user customization"
+    # Set a global variable to make sure we only get called once per session.
+    __tmux_set __tmux_is_initialized
+
+    if functions -q tmux_init
+        tmux_init
+    end
+end
